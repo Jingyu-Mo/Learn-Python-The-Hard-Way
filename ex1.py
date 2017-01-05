45fe524dc8a0bc7df489e760ca45ac23f1aeaa94
Jingyu-Mo <1816273135@qq.com>
2017-01-05 11:46:02 +0800

    Update ex1.py

--- ex1.py
+++ ex1.py
@@ -1,3 +1,4 @@
+# -- coding: utf-8 --
 print "Hello World!"
 print "Hello Again"
 print "I like typing this."
@@ -5,3 +6,6 @@ print 'This is fun.'
 print 'Yay! Printing.'
 print "I'd rather you 'not'."
 print 'I "said" do not touch this.'
+# print 是输出命令，它将单引号或双引号里面的内容打印出来。
+# 如果你想在 Python 脚本里使用非ASCII编码（如中文），那么你就需要在第一行加入声明（见第一行）。
+# 因为我要在注释中使用中文，所以我在第一行添加了非ASCII编码声明，声明包括第一行的全部字符，切记。
