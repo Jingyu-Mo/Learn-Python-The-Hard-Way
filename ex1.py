d21c363961c78fc23aabd6ad33d206af99542b15
unknown <jingyu.mo@yonker.com>
2017-01-05 11:02:34 +0800

    first commit

---
+++ ex1.py
@@ -0,0 +1,14 @@
+# A comment, this is so you can read your program later.
+# Anything after the # is ignored by python.
+print "Hello World!"
+print "Hello Again"
+print "I like typing this."
+print 'This is fun.'
+print 'Yay! Printing.'
+print "I'd rather you 'not'."
+print 'I "said" do not touch this.'
+print "My name is Jing-yu Mo!"
+# You can also use a comment to "disable" or comment out a piece of Code:
+# print "This won't run."
+
+print "This will run."
